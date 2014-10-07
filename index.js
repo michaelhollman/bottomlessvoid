bd93cf5599c799b14f82e02bab2b11d6f89a4f6c
Michael Hollman <hollyfrass@gmail.com>
2014-10-07 17:36:59 -0500

    Added index.html at root and cleared readme

--- index.js
+++ index.js
@@ -17,15 +17,18 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/logs'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  fs.readFile('index.html', encoding='utf8', function (err, data) {
+    if (err) {
+      response.status(503).send(err);
+    } else {
+      response.send(data);
+    }
+  });
 });
 
 app.post('/log', bodyParser.text(), function(req, res) {
 
   var log = '{0} {1}'.format(new Date().toISOString(), req.body.replace(/(\r\n|\n|\r)/gm,'\\n'));
-
-console.log(log);
-
   fs.appendFile('logs/log.txt', '{0}\r\n'.format(log), encoding = 'utf8', function(err) {
     if (err) {
       res.status(503).send(err);

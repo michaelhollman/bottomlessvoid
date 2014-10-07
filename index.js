1ff8986ac62e8345392beff29019b03843696d38
Michael Hollman <hollyfrass@gmail.com>
2014-10-07 17:43:52 -0500

    Added deleting

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + '/logs'));
 app.get('/', function(request, response) {
   fs.readFile('index.html', encoding='utf8', function (err, data) {
     if (err) {
-      response.status(503).send(err);
+      response.status(500).send(err);
     } else {
       response.send(data);
     }
@@ -27,13 +27,22 @@ app.get('/', function(request, response) {
 });
 
 app.post('/log', bodyParser.text(), function(req, res) {
-
   var log = '{0} {1}'.format(new Date().toISOString(), req.body.replace(/(\r\n|\n|\r)/gm,'\\n'));
   fs.appendFile('logs/log.txt', '{0}\r\n'.format(log), encoding = 'utf8', function(err) {
     if (err) {
-      res.status(503).send(err);
+      res.status(500).send(err);
     } else {
       res.status(201).send();
+    }
+  });
+});
+
+app.delete('/log', function(req, res) {
+  fs.writeFile('logs/log.txt', '', encoding = 'utf8', function(err) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send();
     }
   });
 });

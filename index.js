bcce745a0035a947fba9c1aea3f8c20d4384aa15
Michael Hollman <hollyfrass@gmail.com>
2014-11-06 13:29:05 -0600

    Added JSON log file

--- index.js
+++ index.js
@@ -29,24 +29,28 @@ app.get('/', function(request, response) {
 });
 
 app.post('/log', function(req, res) {
-  var log = '{0} {1}'.format(new Date().toISOString(), JSON.stringify(req.body).replace(/(\r\n|\n|\r)/gm,'\\n'));
-  fs.appendFile('logs/log.txt', '{0}\r\n'.format(log), encoding = 'utf8', function(err) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send();
-    }
+
+  var timestamp = new Date().toISOString();
+  var logbody =  JSON.stringify(req.body).replace(/(\r\n|\n|\r)/gm,'\\n');
+
+  var textlog = '{0} {1}'.format(timestamp, logbody);
+  fs.appendFile('logs/log.txt', '{0}\r\n'.format(textlog), encoding = 'utf8');
+
+  fs.readFile('logs/log.json', encoding = 'utf8', function(err, data) {
+    if (!data) data = "{}";
+    json = JSON.parse(data);
+    json[timestamp] = JSON.parse(logbody);
+    jsonStr = JSON.stringify(json);
+    fs.writeFile('logs/log.json', jsonStr, encoding = 'utf8');
   });
+
+  res.status(201).send();
 });
 
 app.delete('/log', function(req, res) {
-  fs.writeFile('logs/log.txt', '', encoding = 'utf8', function(err) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send();
-    }
-  });
+  fs.writeFile('logs/log.txt', '', encoding = 'utf8');
+  fs.writeFile('logs/log.json', '', encoding = 'utf8');
+  res.status(200).send();
 });
 
 app.listen(app.get('port'), function() {
